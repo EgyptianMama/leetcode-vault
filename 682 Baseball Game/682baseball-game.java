d06ca0471fa2b5de075f690be30c8fd322146e83
EgyptianMama <yasharth.social@gmail.com>
2024-10-12 15:38:21 +0000

    Code challenge submission for question: 682 Baseball Game

---
+++ 682 Baseball Game/682baseball-game.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack= new Stack<>();
+        for (int i = 0; i < operations.length; i++) {
+            String op = operations[i];
+            
+            if (op.equals("+")) {
+                int top = stack.pop();
+                int newTop = top + stack.peek();
+                stack.push(top); 
+                stack.push(newTop); 
+            } 
+            else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+            } 
+            else if (op.equals("C")) {
+                stack.pop();
+            } 
+            else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+        int sum=0;
+        while(!stack.isEmpty()){
+            sum= sum + stack.pop();
+        }
+        return sum;
+    }
+}
