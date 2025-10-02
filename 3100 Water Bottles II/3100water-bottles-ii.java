a056206f22d53cf34d2bb40591ec783ef37d8f61
EgyptianMama <yasharth.social@gmail.com>
2025-10-02 23:20:10 +0000

    Code challenge submission for question: 3100 Water Bottles II

---
+++ 3100 Water Bottles II/3100water-bottles-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drink=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange){
+            empty-= numExchange;
+            drink++;
+            empty++;
+            numExchange++;
+        }
+        return drink;
+    }
+}
