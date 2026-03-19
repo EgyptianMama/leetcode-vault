c94fbfeb81a8a7b309482e34c2cc68bd9b2839ad
EgyptianMama <yasharth.social@gmail.com>
2026-03-19 21:56:09 +0000

    Code challenge submission for question: 430 Flatten a Multilevel Doubly Linked List

---
+++ 430 Flatten a Multilevel Doubly Linked List/430flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null) return head;
+
+        Stack<Node> stack = new Stack<>();
+        Node curr = head;
+
+        while (curr != null) {
+            if (curr.child != null) {
+                if (curr.next != null) {
+                    stack.push(curr.next);
+                }
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+            if (curr.next == null && !stack.isEmpty()) {
+                Node temp = stack.pop();
+                curr.next = temp;
+                temp.prev = curr;
+            }
+
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
