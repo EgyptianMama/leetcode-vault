63e4d3a3560bf6b5670c9cf54956f6fbf629530c
EgyptianMama <yasharth.social@gmail.com>
2026-05-17 21:53:58 +0000

    Code challenge submission for question: 1306 Jump Game III

---
+++ 1306 Jump Game III/1306jump-game-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        return dfs(arr, start, new boolean[arr.length]);
+    }
+    
+    private boolean dfs(int[] arr, int curr, boolean[] visited) {
+        if (curr < 0 || curr >= arr.length || visited[curr]) {
+            return false;
+        }
+        if (arr[curr] == 0) {
+            return true;
+        }
+        visited[curr] = true;
+        return dfs(arr, curr + arr[curr], visited) || dfs(arr, curr - arr[curr], visited);
+    }
+}
