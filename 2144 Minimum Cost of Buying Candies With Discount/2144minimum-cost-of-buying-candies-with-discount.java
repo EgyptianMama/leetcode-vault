8c6e308995c174f2a6b707786b48b2dbe8f79309
EgyptianMama <yasharth.social@gmail.com>
2026-06-01 17:44:42 +0000

    Code challenge submission for question: 2144 Minimum Cost of Buying Candies With Discount

---
+++ 2144 Minimum Cost of Buying Candies With Discount/2144minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = cost.length - 1; i >= 0; i--) {
+            if (count != 2) {  
+                sum += cost[i];
+            }
+            count = (count + 1) % 3;
+        }
+
+        return sum;
+    }
+}
